c3f52f1c840bc79c5333fab76381664c169aa235
A01026608 <89270787+A01026608@users.noreply.github.com>
2021-08-24 16:21:42 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    n1= int(input('Ingresa el primer número:'))
+    n2= int(input('Ingresa el segundo número:'))
+    n3= int(input('Ingresa el tercer número:'))
+    
+    if n2<n1>n3:
+        print(n1)
+    elif n1<n2>n3:
+        print(n2)
+    elif n1<n3>n2:
+        print(n3)
+    
 if __name__=='__main__':
     main()
